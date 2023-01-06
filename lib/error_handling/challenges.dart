5c81251679608051e14dc88c2fa6307ec848d552
Inan Mahmud <inan.mahmud@cefalo.com>
2023-01-06 12:03:33 +0600

    error_handling done

--- lib/error_handling/challenges.dart
+++ lib/error_handling/challenges.dart
@@ -1,4 +1,11 @@
 void main() {
+  challenge1();
+  challenge2();
+}
+
+///Challenge -1 Double the Fun
+
+void challenge1() {
   try {
     doubleTheFun();
   } on FormatException catch (e) {
@@ -6,11 +13,28 @@ void main() {
   }
 }
 
-///Challenge -1 Double the Fun
-
 void doubleTheFun() {
   final numbers = ['3', '1E+8', '1.25', 'four', '-0.01', 'Nan', 'Infinity'];
   for (var number in numbers) {
     print(double.parse(number));
   }
+}
+
+void challenge2() {
+  isValidPostalCode(1234);
+}
+
+void isValidPostalCode(int postalCode) {
+  String postalCodeString = postalCode.toString();
+  if (postalCodeString.length != 5) {
+    throw InvalidPostalCodeException('Postal Code must be of 5 digits');
+  }
+}
+
+///Challenge 2 - Five Digits, No More, No Less
+
+class InvalidPostalCodeException implements Exception {
+  final String message;
+
+  InvalidPostalCodeException(this.message);
 }
